8a53f15b93447c43d9f3bb98d6beeb1bfbb87a9e
蔡斌伟 <caibinwei@jiudaotech.com>
2022-08-02 17:07:43 +0800

    DispatchServlet解析

--- MyBoot3Web/src/main/java/com/jiudaotech/myboot3web/controller/HelloController.java
+++ MyBoot3Web/src/main/java/com/jiudaotech/myboot3web/controller/HelloController.java
@@ -1,14 +1,39 @@
 package com.jiudaotech.myboot3web.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
 	@RequestMapping("/flow1.png")
-	public String hello() {
-		return "flow0.png";
+	public String hello(@RequestParam("name") String name) {
+		return name;
 	}
 
+	@GetMapping(value = "/user")
+	public String getUser(){
+		return "GET-张三";
+	}
+
+	@PostMapping(value = "/user")
+	public String saveUser(){
+		return "POST-张三";
+	}
+
+
+	@PutMapping(value = "/user")
+	public String putUser(){
+		return "PUT-张三";
+	}
+
+	@RequestMapping(value = "/user",method = RequestMethod.DELETE)
+	public String deleteUser(){
+		return "DELETE-张三";
+	}
 }
